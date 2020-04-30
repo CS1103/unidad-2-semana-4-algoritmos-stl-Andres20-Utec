380738a9a7c5615933d834bc716bb1c1e222b166
Andres20-Utec <andres.riveros@utec.edu.pe>
2020-04-30 09:33:37 -0500

    commit

--- ubaProblems/funciones.h
+++ ubaProblems/funciones.h
@@ -9,9 +9,30 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <utility>
 using namespace std;
 
-int amountSwap(vector<int> vec){
+//UBA 146
+void nextPermutation(){
+    string word;
+    do{
+        cin >> word;
+        if(next_permutation(word.begin(),word.end()))
+            cout << word << endl;
+        else if(word.at(0) == '#') {
+            cout << endl;
+            return;
+        }
+        else{
+            cout << "No sucesor" << endl;
+        }
+    }
+    while(word.at(0) != '#');
+}
+
+// UBA299
+template<typename T>
+int amountOfSwaps(vector<T> vec){
     int count = 0;
     for(int i = 0; i < vec.size(); ++i){
         for(int j = 0; j < vec.size()- 1; ++j){
@@ -25,15 +46,16 @@ int amountSwap(vector<int> vec){
     }
     return count;
 }
-
-void displayOrder(vector<int> vecI, vector<double> vecD){
+// UBA 482
+template<typename T1, typename T2>
+void displayOrder(vector<T1> vecI, vector<T2> vecD){
     for(int i = 0; i < vecI.size(); ++i){
         for(int j = 0, f = 0; j < vecI.size()- 1;++f ,++j){
             if(vecI.at(j) > vecI.at(j + 1)){
-                int temp = vecI.at(j);
+                T1 temp = vecI.at(j);
                 vecI.at(j) = vecI.at(j + 1);//Posición inicial
                 vecI.at(j + 1) = temp; // Posición siguiente
-                int tempS = vecD.at(f);
+                T2 tempS = vecD.at(f);
                 vecD.at(f) = vecD.at(f + 1);//Posición inicial
                 vecD.at(f + 1) = tempS; // Posición siguiente
             }
@@ -43,5 +65,32 @@ void displayOrder(vector<int> vecI, vector<double> vecD){
         cout << vec << " ";
     }
     cout << endl;
+}
+
+// UBA 612
+bool compareInterval(pair<string,int> i1, pair<string,int> i2)
+{
+    return (i1.second < i2.second);
+}
+int AmountOfSwapsStr(string word){
+    int count = 0;
+    for(int j = 0; j < word.size();++j){
+        for(int i = 0; i < word.size() - 1;++i){
+            if(static_cast<int>(word[i]) > static_cast<int>(word[i + 1])){
+                string before = word.substr(0, i);
+                string after = word.substr(i+2,word.size()-i-1);
+                word = before + word[i + 1] + word[i] + after;
+                count++;
+            }
+        }
+    }
+    return count;
+}
+void displayOrder(vector<pair<string,int>> wordAndAmountOfSwaps){
+    sort(wordAndAmountOfSwaps.begin(),wordAndAmountOfSwaps.end(),compareInterval);
+    cout << endl;
+    for(auto dniWord : wordAndAmountOfSwaps){
+        cout << dniWord.first <<endl;
+    }
 }
 #endif //UBAPROBLEMS_FUNCIONES_H
--- ubaProblems/main.cpp
+++ ubaProblems/main.cpp
@@ -2,49 +2,61 @@
 
 int main() {
 
-  string word;
-  cin >> word;
-  int size1;
-  if(next_permutation(word.begin(),word.end()))
-    cout << word << endl;
-  else{
-    cout << "No sucesor" << endl;
-  }
-  size_t size;
-  vector<int> answer;
-  cout << "Cantidad de veces: "; cin >> size;
-  for(int i= 0; i < size;++i)
-  {
-    vector<int> vec;
-    int size2;
-    cin>> size2;
-    for(int j=0; j < size2; ++j){
-      int elem;
-      cin >> elem;
-      vec.push_back(elem);
+    // UBA 146
+    nextPermutation();
+
+    //UBA 299
+    size_t size;
+    vector<int> answer;
+    cout << "Cantidad de veces: "; cin >> size;
+    for(int i= 0; i < size;++i)
+    {
+        vector<int> vec;
+        int size2;
+        cin>> size2;
+        for(int j=0; j < size2; ++j){
+            int elem;
+            cin >> elem;
+            vec.push_back(elem);
+        }
+        answer.push_back(amountOfSwaps(vec));
     }
-    answer.push_back(amountSwap(vec));
-  }
-  for(int i = 0; i < answer.size();++i){
-    cout << "Optimal train swapping takes " << answer.at(i) << " swaps"<< endl;
-  }
+    for(int i = 0; i < answer.size();++i){
+        cout << "Optimal train swapping takes " << answer.at(i) << " swaps"<< endl;
+    }
 
-  string text = "3 1 2 5 4";
-  vector <int> vecI;
-  stringstream ss(text);
-  int value;
-  while(ss >> value){
-    vecI.push_back(value);
-  }
-  string text1 = "32.0 54.7 -2 11.25 32.1";
-  double value2;
-  stringstream ss1(text1);
-  vector<double> vecD;
-  while(ss1 >> value2){
-    vecD.push_back(value2);
-  }
+// UBA 482
+    string text = "3 1 2 5 4";
+    vector <int> vecI;
+    stringstream ss(text);
+    int value;
+    while(ss >> value){
+        vecI.push_back(value);
+    }
+    string text1 = "32.0 54.7 -2 11.25 32.1";
+    double value2;
+    stringstream ss1(text1);
+    vector<double> vecD;
+    while(ss1 >> value2){
+        vecD.push_back(value2);
+    }
+    displayOrder(vecI, vecD);
 
-  displayOrder(vecI, vecD);
-  return 0;
-
+// UBA 612
+    int sizeWord;
+    int numWord;
+    cin >> sizeWord >> numWord;
+    vector<pair<string,int>> wordAndAmountOfSwaps;
+    while(numWord--){
+        string word;
+        cin >> word;
+        while(word.length()!= sizeWord){
+            cout << "Enter a word with " << sizeWord << "letters: ";
+            cin >> word;
+        }
+        wordAndAmountOfSwaps.push_back(pair<string,int>{word,AmountOfSwapsStr(word)});
+    }
+    displayOrder(wordAndAmountOfSwaps);
+    cout << endl;
+    return 0;
 }
